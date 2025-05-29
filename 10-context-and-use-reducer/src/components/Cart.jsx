86401c62a9cb76e72921283e47cd16bae1a4e96b
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2025-05-29 21:24:37 +0700

    fix: rename folder

--- 10-context-and-use-reducer/src/components/Cart.jsx
+++ 10-context-and-use-reducer/src/components/Cart.jsx
@@ -1,8 +1,10 @@
 import { useContext } from 'react';
+// import { use } from 'react';
 
 import { CartContext } from '../store/shopping-cart-context.jsx';
 
 export default function Cart() {
+  // const { items, updateItemQuantity } = use(CartContext);
   const { items, updateItemQuantity } = useContext(CartContext);
 
   const totalPrice = items.reduce(
