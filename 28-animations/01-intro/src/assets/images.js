ea8167864de5d9d681a026bc0540f1031e7e8e7a
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2025-06-08 14:36:24 +0700

    feat: add 28-animations

---
+++ 28-animations/01-intro/src/assets/images.js
@@ -0,0 +1,17 @@
+import constructingImg from "./constructing.png";
+import cookingImg from "./cooking.png";
+import familyTimeImg from "./family-time.png";
+import playingGuitarImg from "./playing-guitar.png";
+import romanticWalkImg from "./romantic-walk.png";
+import workingOnComputerImg from "./working-on-computer.png";
+import workingOutImg from "./working-out.png";
+
+export default [
+    { src: constructingImg, alt: "Person working on some furniture." },
+    { src: cookingImg, alt: "Person cooking a meal." },
+    { src: familyTimeImg, alt: "Family spending time together." },
+    { src: playingGuitarImg, alt: "Person playing the guitar." },
+    { src: romanticWalkImg, alt: "Couple walking together in the moonshine." },
+    { src: workingOnComputerImg, alt: "Person doing work on a computer." },
+    { src: workingOutImg, alt: "Person working out." },
+];
